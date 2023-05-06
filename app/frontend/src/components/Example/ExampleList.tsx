69a8883edbf55efe40bf3c2adec191f7edc5bcaf
limonaluna <46324520+limonaluna@users.noreply.github.com>
2023-05-06 13:39:03 +0200

    modified some files

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What are the vegetarian lunch options?",
+        value: "What are the vegetarian lunch options?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "Which main dish has the least amount of carbs?", value: "Which main dish has the least amount of carbs?" },
+    { text: "What is the dessert on Friday?", value: "What is the dessert on Friday?" }
 ];
 
 interface Props {
